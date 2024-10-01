87ff452dbcdc5f5c3b9a093e402ddafc571c1a5f
Luciano da Conceição Junior <luciano@5aliens.com>
2024-10-01 17:38:27 -0300

    Adjusting seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-organization = Organization.where(name: '5Aliens').first_or_create!(api_key: SecureRandom.urlsafe_base64(64))
+organization = Organization.where(name: '5 Aliens').first_or_create!(api_key: SecureRandom.urlsafe_base64(64))
 project = Project.where(name: 'Hero', organization:).first_or_create!
 Leaderboard.where(name: 'Weekly', period_type: :weekly, sort_type: :descent, row_type: :best, project:).first_or_create!
 Leaderboard.where(name: 'Overall', period_type: :overall, sort_type: :descent, row_type: :best, project:).first_or_create!
